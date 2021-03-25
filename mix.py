abe949df4e3443e9985c5f02d9701f0d9dc93306
Chris DiMaio <chrisdimaio@gmail.com>
2021-03-25 00:46:44 -0400

    updated and removed comments

--- mix.py
+++ mix.py
@@ -2,17 +2,11 @@ def calculate(n, p, k, fn, fp, fk, coverage, area):
     p_coverage = coverage * (p/n)
     k_coverage = coverage * (k/n)
 
-    # coverage/(n/100) has to come into play somewhere
-    # concentration might be fx/1000
-    # and we need an n concetration
     n_concentration = fn/100
     p_concentration = fp/100
     k_concentration = fk/100
-    # Original was
-    # p_concentration = fp/fn
-    # k_concentration = fk/fn
 
-    # p and k coverage adjusted for concentration.
+    # coverage adjusted for concentration.
     n_adjusted = coverage/n_concentration
     p_adjusted = p_coverage/p_concentration
     k_adjusted = k_coverage/k_concentration
