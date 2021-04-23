60e7b9284dfb60f329184c7afd4dcb3ed564c97b
Chris DiMaio <chrisdimaio@gmail.com>
2021-04-22 20:19:10 -0400

    progress

--- cli.py
+++ cli.py
@@ -1,32 +1,36 @@
+from mix import calculate_coverage, calculate_weight
 import click
 
-from mix import calculate
 
+# @ click.command()
+# @ click.option('--mode', required=True, prompt='What mode?', type=int)
+# @ click.option('--n', required=True, prompt='Whats your desired N ratio?', type=int)
+# @ click.option('--p', required=True, prompt='Whats your desired P ratio?', type=int)
+# @ click.option('--k', required=True, prompt='Whats your desired K ratio?', type=int)
+# @ click.option('--fn', required=True, prompt='Your fertilizer N content?', type=int)
+# @ click.option('--fp', required=True, prompt='Your fertilizer P content?', type=int)
+# @ click.option('--fk', required=True, prompt='Your fertilizer K content?', type=int)
+# @ click.option('--coverage', required=True, prompt='What\'s your nitrogen coverage in pounds per 1,000 ft square?', type=float)
+# @ click.option('--area', required=True, prompt='Coverage area in square feet?', type=int, default=1000)
+# def cli(n, p, k, fn, fp, fk, coverage, area):
+#     click.echo(f"Desired ratio {n}-{p}-{k}")
+#     click.echo(
+#         (f"Fertilizer content:\n\tNitrogen={fn}\n\tPhosphorus={fp}\n\tPotassium={fk}"))
+#     click.echo(f"Coverage is {coverage} lbs per 1,000 square feet")
 
-@ click.command()
-@ click.option('--n', required=True, prompt='Whats your desired N ratio?', type=int)
-@ click.option('--p', required=True, prompt='Whats your desired P ratio?', type=int)
-@ click.option('--k', required=True, prompt='Whats your desired K ratio?', type=int)
-@ click.option('--fn', required=True, prompt='Your fertilizer N content?', type=int)
-@ click.option('--fp', required=True, prompt='Your fertilizer P content?', type=int)
-@ click.option('--fk', required=True, prompt='Your fertilizer K content?', type=int)
-@ click.option('--coverage', required=True, prompt='What\'s your nitrogen coverage in pounds per 1,000 ft square?', type=float)
-@ click.option('--area', required=True, prompt='Coverage area in square feet?', type=int, default=1000)
-def cli(n, p, k, fn, fp, fk, coverage, area):
-    click.echo(f"Desired ratio {n}-{p}-{k}")
-    click.echo(
-        (f"Fertilizer content:\n\tNitrogen={fn}\n\tPhosphorus={fp}\n\tPotassium={fk}"))
-    click.echo(f"Coverage is {coverage} lbs per 1,000 square feet")
+#     coverages = calculate_coverage(n, p, k, fn, fp, fk, coverage, area)
+#     nc = round(coverages[0], 3)
+#     pc = round(coverages[1], 3)
+#     kc = round(coverages[2], 3)
 
-    coverages = calculate(n, p, k, fn, fp, fk, coverage, area)
-    nc = round(coverages[0], 3)
-    pc = round(coverages[1], 3)
-    kc = round(coverages[2], 3)
-
-    click.echo(
-        f"Coverages in pounds for {area} square feet.\n\tNitrogen: {nc} lbs\n\tPhosphorus: {pc} lbs\n\tPotassium: {kc} lbs")
+#     click.echo(
+#         f"Coverages in pounds for {area} square feet.\n\tNitrogen: {nc} lbs\n\tPhosphorus: {pc} lbs\n\tPotassium: {kc} lbs")
 
 
 if __name__ == '__main__':
-    cli()
-    # calculate(4, 1, 2, 45, 20, 60, .9, 5000)
+    n, p, k = calculate_weight(12, 4, 3, 46, 20, 60, 10)
+    print(n, p, k)
+    print(sum((n, p, k)))
+    print(n*.46, p*.20, k*.6)
+    # cli()
+    # calculate_coverage(4, 1, 2, 45, 20, 60, .9, 5000)
--- mix.py
+++ mix.py
@@ -1,10 +1,12 @@
-def calculate(n, p, k, fn, fp, fk, coverage, area):
+def calculate_concentrations(fn, fp, fk):
+    return (fn/100, fp/100, fk/100)
+
+
+def calculate_coverage(n, p, k, fn, fp, fk, coverage, area):
     p_coverage = coverage * (p/n)
     k_coverage = coverage * (k/n)
 
-    n_concentration = fn/100
-    p_concentration = fp/100
-    k_concentration = fk/100
+    n_concentration, p_concentration, k_concentration = fn/100, fp/100, fk/100
 
     # coverage adjusted for concentration.
     n_adjusted = coverage/n_concentration
@@ -18,3 +20,44 @@ def calculate(n, p, k, fn, fp, fk, coverage, area):
     k_scaled = k_adjusted * scalar
 
     return (n_scaled, p_scaled, k_scaled)
+
+
+def calculate_weight(n, p, k, fn, fp, fk, weight):
+    # 100 parts for each of the parts n p k.
+    total_parts = n + p + k
+
+    n_percent = n / total_parts
+    p_percent = p / total_parts
+    k_percent = k / total_parts
+
+    n_weight = weight * n_percent
+    p_weight = weight * p_percent
+    k_weight = weight * k_percent
+
+    total = n_weight + p_weight + k_weight
+
+    return weight * n_percent, weight * p_percent, weight * k_percent
+
+# def calculate_weight(n, p, k, fn, fp, fk, weight):
+#     # n_coverage = weight / n
+#     p_coverage = 1 * (p/n)
+#     k_coverage = 1 * (k/n)
+
+#     n_concentration, p_concentration, k_concentration = fn/100, fp/100, fk/100
+
+#     # coverage adjusted for concentration.
+#     n_adjusted = 1/fn
+#     p_adjusted = 1/fp
+#     k_adjusted = 1/fk
+
+#     print(n_adjusted, p_adjusted, k_adjusted)
+
+#     total_adjusted = n_adjusted + p_adjusted + k_adjusted
+
+#     n_percent = n_adjusted / total_adjusted
+#     p_percent = p_adjusted / total_adjusted
+#     k_percent = k_adjusted / total_adjusted
+
+#     print(n_percent, p_percent, k_percent)
+
+#     return weight * n_percent, weight * p_percent, weight * k_percent
